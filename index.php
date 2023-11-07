13cf47b36f21d0db31aaa5fa494ce4043670bf7f
Hasaan Muhammed <hasaan.muhamm3d@gmail.com>
2023-11-07 19:43:15 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PoC: By: BugSwagger</title>
+</head>
+<body>
+    <p>PoC: By: BugSwagger.</p>
+</body>
+</html>
